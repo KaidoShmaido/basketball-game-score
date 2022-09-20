61b4c6a7787672cb0d2697a16b77f97a0231f2fa
KaidoShmaido <113863840+KaidoShmaido@users.noreply.github.com>
2022-09-21 00:28:45 +0100

    basketball game score via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+let button = document.getElementById('home')
+let score = 0
+function plus_one(){
+    score+=1
+    button.textContent=score
+}
+function plus_two(){
+    score+=2
+    button.textContent=score
+}
+function plus_three(){
+    score+=3
+    button.textContent=score
+}
+
+
+
+let button1 = document.getElementById('guest')
+let guest = 0
+function plus1(){
+    guest+=1
+    button1.textContent=guest
+}
+function plus2(){
+    guest+=2
+    button1.textContent=guest
+}
+function plus3(){
+    guest+=3
+    button1.textContent=guest
+}
